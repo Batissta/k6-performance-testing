f976dfe9dd2a26e843bbeff9dff2016e35653499
Batissta <francinaldofilho05@gmail.com>
2025-08-20 10:31:42 -0300

    fix: importações ajustadas

--- simulation/signUp.js
+++ simulation/signUp.js
@@ -1,8 +1,8 @@
 import { group, sleep } from "k6";
-import { geradorPayload } from "../data/dynamic/signUpDataFactory";
-import { signUpRequest } from "../support/services/requests/signUpRequest";
+import { geradorPayload } from "../data/dynamic/signUpDatapool.js";
+import signUpRequest from "../support/services/requests/signUpRequest.js";
 
-export function criarUsuario() {
+export function signUp() {
   group("Cadastro de usuário", () => {
     const payload = geradorPayload();
     signUpRequest(payload);
