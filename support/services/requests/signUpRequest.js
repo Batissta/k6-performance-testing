10e5fb6c75e715d23667e60f472c06571d9af12e
Batissta <francinaldofilho05@gmail.com>
2025-08-20 12:27:07 -0300

    feat: testando a execução através de uma url direcionada ao docker-host

--- support/services/requests/signUpRequest.js
+++ support/services/requests/signUpRequest.js
@@ -2,7 +2,7 @@ import { environment } from "../../base/environment.js";
 import { checkStatusCode } from "../../base/checks.js";
 import { restService } from "../baseRest.js";
 
-const service = new restService(environment.BASE_URI);
+const service = new restService(environment.BASE_URI_DOCKER);
 
 export default function signUpRequest(payload) {
   const res = service.post("/signup", payload);
