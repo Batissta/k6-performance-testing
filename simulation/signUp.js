95f0c939399c7817ae4f37aa13bd7099f8f6923e
Batissta <francinaldofilho05@gmail.com>
2025-08-28 14:39:34 -0300

    feat: implementando nova métrica ao teste para mandar um sinal de fumaça de teste ativo

--- simulation/signUp.js
+++ simulation/signUp.js
@@ -1,9 +1,13 @@
 import { group, sleep } from "k6";
+import { Counter } from "k6/metrics";
 import { geradorPayload } from "../data/dynamic/signUpDatapool.js";
 import signUpRequest from "../support/services/requests/signUpRequest.js";
 
+const TestActive = new Counter("test_active");
+
 export function signUp() {
   group("Cadastro de usuário", () => {
+    TestActive.add(1);
     const payload = geradorPayload();
     signUpRequest(payload);
     sleep(1);
